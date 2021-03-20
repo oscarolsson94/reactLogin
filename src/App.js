1c694f294ca5af4722f4d8f7b3522e3d290cde28
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-20 13:34:55 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Navigation from './components/Navigation';
+import Login from './components/Login';
+import Register from './components/Register';
+import { BroserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-      <div className="App">    
+    return (
+    <Router>
+        <div className="App">    
+            <Navigation />
+          
+            <div className="auth-wrapper">
+                    <div className="auth-inner" >
+                        <Switch>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                            <Route path="/login">
+                                <Login />
+                            </Route>
+                            <Route path="/register">
+                                <Register />
+                            </Route>
+                        </Switch>    
+                </div>
+            </div>
 
-          <div className="auth-wrapper">
-              <div className="auth-inner" >
-                    <Home />
-              </div>
-          </div>
-
-    </div>
+            </div>
+    </Router>
   );
 }
 
