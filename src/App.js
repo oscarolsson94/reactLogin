3c3d52307c2f83ef8b56fd4b02064d38add257f5
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-20 13:06:53 +0100

    install bootstrap and create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
+          <nav className="navbar navbar-expand navbar-light fixed-top" >
+              <div className="conatiner">
+                  <a href="" className="navbar-brand" >Home</a>
+                  <div className="collapse navbar-collapse">
+                      <ul className="navbar-nav ml-auto">
+                          <li className="nav-item">
+                              <a href="" className="nav-link" >Login</a>
+                          </li>
+                          <li className="nav-item">
+                              <a href="" className="nav-link" >Sign up</a>
+                          </li>
+                      </ul>
+                  </div>
+            </div>
+          </nav>
 
     </div>
   );
