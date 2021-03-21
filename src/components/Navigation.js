5768d1220c3e3453756f734f82b1b3fd8997e12d
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-21 15:56:10 +0100

    conditional render

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,19 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navigation = () => {
+
+    if (user) { //if user is not an empty object, display this
+        
+    }
+
     return (
+
         <nav className="navbar navbar-expand navbar-light fixed-top" >
               <div className="container">
                   <Link to="/" className="navbar-brand" >Home</Link>
                   <div className="collapse navbar-collapse">
-                      <ul className="navbar-nav ml-auto">
-                          <li className="nav-item">
-                              <Link to="/login" className="nav-link" >Login</Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link to="/register" className="nav-link" >Sign up</Link>
-                          </li>
-                      </ul>
+                      
                   </div>
                 </div>
         </nav>
