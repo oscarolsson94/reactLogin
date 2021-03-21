569c61f07041bedb3304b85c19f45d23f92984ae
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-21 12:51:23 +0100

    state

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,21 @@ import React, { useEffect } from 'react'
 
 const Home = () => {
 
+    state = {}; //use the global state
+
+    useEffect(() => {
+
     const config = {
         headers: {
             Authorization: "Bearer " + localStorage.getItem('token');
         }
     };
 
-    useEffect(() => {
-        axios.get('user', config) //hit user route /users or /validate-token, needs a route that returns user data based on token
+    axios.get('user', config) //hit user route /users or /validate-token, needs a route that returns user data based on token
             .then(res => {
-            console.log(res);
+                setState({ // use the global method
+                    user: res.data
+                });
             },
                 err => {
                 console.log(err);
